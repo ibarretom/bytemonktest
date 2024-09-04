35f4a75f43c6323db270fdbb5491d208fe341757
ibarretom <igorbm.23@gmail.com>
2024-09-04 19:33:06 -0300

    init

---
+++ bytemonk/src/main/java/com/bytemonk/securityincidents/SecurityincidentsApplication.java
@@ -0,0 +1,13 @@
+package com.bytemonk.securityincidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityincidentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityincidentsApplication.class, args);
+	}
+
+}
