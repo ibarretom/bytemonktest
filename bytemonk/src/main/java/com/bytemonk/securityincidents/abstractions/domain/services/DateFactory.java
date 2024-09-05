e5db3fc5f7ad322973a6a430ea797cefd04e1435
ibarretom <igorbm.23@gmail.com>
2024-09-04 21:26:48 -0300

    Date factory added

---
+++ bytemonk/src/main/java/com/bytemonk/securityincidents/abstractions/domain/services/DateFactory.java
@@ -0,0 +1,22 @@
+package com.bytemonk.securityincidents.abstractions.domain.services;
+
+import java.util.Date;
+
+public class DateFactory {
+    private static final int TO_HOURS = 24;
+    private static final int TO_MINUTES = 60;
+    private static final int TO_SECONDS = 60;
+    private static final int TO_MILLISECONDS = 1000;
+
+    public static Date now() {
+        return new Date();
+    }
+
+    public static Date add(Date aDate, int anIncrement) {
+        return new Date(parseToMilliseconds(aDate, anIncrement));
+    }
+
+    private static long parseToMilliseconds(Date aDate, int anIncrement) {
+        return aDate.getTime() + (long) anIncrement * TO_HOURS * TO_MINUTES * TO_SECONDS * TO_MILLISECONDS;
+    }
+}
---
+++ bytemonk/src/test/java/com/bytemonk/securityincidents/abstractions/domain/DateFactoryTest.java
@@ -0,0 +1,27 @@
+package com.bytemonk.securityincidents.abstractions.domain;
+
+import com.bytemonk.securityincidents.abstractions.domain.services.DateFactory;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class DateFactoryTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, -1, 2, -2})
+    void should_add_the_right_amount_of_time(int anIncrement) {
+        var now = DateFactory.now();
+        var aResult = DateFactory.add(now, anIncrement);
+
+        assertEquals(getMilliseconds(now, anIncrement), aResult.getTime());
+    }
+
+    private long getMilliseconds(Date aDate, int anIncrement) {
+        return aDate.getTime() + (long) anIncrement * 24 * 60 * 60 * 1000;
+    }
+}
