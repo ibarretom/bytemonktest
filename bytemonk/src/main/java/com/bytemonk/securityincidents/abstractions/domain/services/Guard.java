69622f056520ef4251a0f0221f0020d57586fdeb
ibarretom <igorbm.23@gmail.com>
2024-09-04 21:15:02 -0300

    Guard clause added. String smaller then

--- bytemonk/src/main/java/com/bytemonk/securityincidents/abstractions/domain/services/Guard.java
+++ bytemonk/src/main/java/com/bytemonk/securityincidents/abstractions/domain/services/Guard.java
@@ -2,6 +2,8 @@ package com.bytemonk.securityincidents.abstractions.domain.services;
 
 import com.bytemonk.securityincidents.abstractions.domain.exceptions.DomainException;
 
+import java.text.MessageFormat;
+
 public class Guard {
     public static class Against {
         public static String NullOrEmpty(String aValue) throws DomainException {
@@ -10,5 +12,18 @@ public class Guard {
 
             return aValue;
         }
+        
+        public static String SmallerThen(int aLength, String aValue) throws DomainException {
+            NullOrEmpty(aValue);
+
+            if (aLength > aValue.length()) {
+                String aMessage = "Value {0} must be bigger then {1} characters";
+                throw new DomainException(MessageFormat.format(aMessage, aValue, aLength));
+            }
+
+            return aValue;
+        }
+
+       
     }
 }
--- bytemonk/src/test/java/com/bytemonk/securityincidents/abstractions/services/GuardTest.java
+++ bytemonk/src/test/java/com/bytemonk/securityincidents/abstractions/services/GuardTest.java
@@ -22,13 +22,34 @@ public class GuardTest {
     }
 
     @ParameterizedTest
-    @MethodSource("invalidNamesProvider")
+    @MethodSource("invalidEmptyFieldsProvider")
     void should_reject_an_invalid_string_for_not_null_or_empty(String anInvalidString) throws DomainException {
         assertThrows(DomainException.class, () -> Guard.Against.NullOrEmpty(anInvalidString));
     }
 
-    private static Stream<String> invalidNamesProvider() {
+    private static Stream<String> invalidEmptyFieldsProvider() {
         return Stream.of(""," ", null);
     }
 
+    @Test
+    void should_accept_a_valid_string_in_certain_range() throws DomainException {
+        var aString = "Nori";
+
+        assertEquals(aString, Guard.Against.SmallerThen(0, aString));
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidEmptyFieldsProvider")
+    void should_throw_for_empty_values(String anEmptyValue) {
+        assertThrows(DomainException.class, () -> Guard.Against.SmallerThen(2, anEmptyValue));
+    }
+
+    @Test
+    void should_throw_for_invalid_range() {
+        var aSmallWord = "dwarf";
+
+        var aSize = aSmallWord.length() + 1;
+
+        assertThrows(DomainException.class, () -> Guard.Against.SmallerThen(aSize, aSmallWord));
+    }
 }
