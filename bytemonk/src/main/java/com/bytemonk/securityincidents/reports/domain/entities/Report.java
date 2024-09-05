e8bc97409337791b8f4f24edc0ba1e8c54b09753
ibarretom <igorbm.23@gmail.com>
2024-09-05 02:10:41 -0300

    refactory: Report as his on AggregateRoot

--- bytemonk/src/main/java/com/bytemonk/securityincidents/reports/domain/entities/Report.java
+++ bytemonk/src/main/java/com/bytemonk/securityincidents/reports/domain/entities/Report.java
@@ -1,13 +1,13 @@
 package com.bytemonk.securityincidents.reports.domain.entities;
 
-import com.bytemonk.securityincidents.abstractions.domain.Entity;
+import com.bytemonk.securityincidents.abstractions.domain.AggregateRoot;
 import com.bytemonk.securityincidents.reports.domain.valueobjects.Description;
 import com.bytemonk.securityincidents.reports.domain.valueobjects.Title;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-public sealed abstract class Report extends Entity permits Incident {
+public sealed abstract class Report extends AggregateRoot permits Incident {
     private final Title title;
 
     @Setter
