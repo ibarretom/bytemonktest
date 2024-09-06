85f6f020a0a2f63e46ba26e34d73dc7e9c025844
ibarretom <igorbm.23@gmail.com>
2024-09-05 22:54:00 -0300

    refactor: Add AuthorizationFilter for handling user authentication and authorization

---
+++ bytemonk/src/main/java/com/bytemonk/securityincidents/abstractions/adapters/presentation/AuthorizationFilter.java
@@ -0,0 +1,58 @@
+package com.bytemonk.securityincidents.abstractions.adapters.presentation;
+
+import com.bytemonk.securityincidents.abstractions.domain.exceptions.ValidationException;
+import com.bytemonk.securityincidents.reports.application.domain.valueobjects.AuthenticatedUserRequest;
+import com.bytemonk.securityincidents.users.IUserRepository;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AuthorizationFilter implements Filter {
+    private final IUserRepository anUserRepository;
+
+    @Autowired
+    AuthorizationFilter(IUserRepository anUserRepository) {
+        this.anUserRepository = anUserRepository;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        var anHttpRequest = (HttpServletRequest) servletRequest;
+
+        var anAuthenticatedUser = getUserCredentials(anHttpRequest);
+
+        var anHttpResponse = (HttpServletResponse) servletResponse;
+
+        if (anAuthenticatedUser == null) {
+            anHttpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        var anUser = anUserRepository.findByUsername(anAuthenticatedUser.getDomainUsername());
+
+        if (anUser == null) {
+            anHttpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+
+        anHttpRequest.setAttribute("authenticatedUser", anUser);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private AuthenticatedUserRequest getUserCredentials(HttpServletRequest aRequest) {
+
+        try {
+            var anUsername = aRequest.getHeader("X-Authorization-Username");
+            var aPassword = aRequest.getHeader("X-Authorization-Password");
+
+            return new AuthenticatedUserRequest(anUsername, aPassword);
+        }catch (ValidationException ex) {
+            return null;
+        }
+    }
+}
