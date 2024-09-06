4f4b1954d0210c29d76171e639873dd41b745160
ibarretom <igorbm.23@gmail.com>
2024-09-06 02:25:39 -0300

    refactor: Update IJpaAccess method parameter names for consistency

--- bytemonk/src/main/java/com/bytemonk/securityincidents/reports/adapters/persistence/database/IJpaAccess.java
+++ bytemonk/src/main/java/com/bytemonk/securityincidents/reports/adapters/persistence/database/IJpaAccess.java
@@ -7,8 +7,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface IJpaAccess extends JpaRepository<Report, Long> {
     @Query(value = "SELECT i FROM Report i WHERE i.title = :title and i.user.username = :username")
-    Report findByTitle(@Param("title") String aTitle, @Param("username") String aUsername);
+    Report findByTitle(@Param("title") String aTitle, @Param("username") String anUsername);
 
     @Query(value = "SELECT r FROM Report r WHERE r.id = :id AND r.user.username = :username")
-    Report findByIncidentId(@Param("id") Long aId, @Param("username") String aUsername);
+    Report findByIncidentId(@Param("id") Long aId, @Param("username") String anUsername);
 }
