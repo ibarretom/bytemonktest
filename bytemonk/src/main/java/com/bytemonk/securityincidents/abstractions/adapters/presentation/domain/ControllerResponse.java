bea134b7682339f39d42f5a4f8d4b3191e2bb5ae
ibarretom <igorbm.23@gmail.com>
2024-09-05 22:52:59 -0300

    refactor: Update ControllerResponse to adhere to consistent naming conventions

---
+++ bytemonk/src/main/java/com/bytemonk/securityincidents/abstractions/adapters/presentation/domain/ControllerResponse.java
@@ -0,0 +1,31 @@
+package com.bytemonk.securityincidents.abstractions.adapters.presentation.domain;
+
+import com.bytemonk.securityincidents.abstractions.application.domain.valueobjects.ApplicationResponse;
+
+import java.util.ArrayList;
+
+public class ControllerResponse<T> {
+    Iterable<T> Response;
+    String Message;
+
+    private ControllerResponse(Iterable<T> Response) {
+        this.Response = Response;
+        this.Message = "";
+    }
+
+    private ControllerResponse(String Message) {
+        this.Message = Message;
+        this.Response = new ArrayList<>();
+    }
+
+    public static <TResponse> ControllerResponse<TResponse> create(ApplicationResponse<TResponse> anApplicationResponse) {
+        if (!anApplicationResponse.isSuccess())
+            return new ControllerResponse<TResponse>(anApplicationResponse.getMessage());
+
+        var aResponseList = new ArrayList<TResponse>();
+        aResponseList.add(anApplicationResponse.getResult());
+
+        return new ControllerResponse<TResponse>(aResponseList);
+    }
+
+}
--- bytemonk/src/main/java/com/bytemonk/securityincidents/abstractions/application/domain/valueobjects/ApplicationResponse.java
+++ bytemonk/src/main/java/com/bytemonk/securityincidents/abstractions/application/domain/valueobjects/ApplicationResponse.java
@@ -1,6 +1,7 @@
 package com.bytemonk.securityincidents.abstractions.application.domain.valueobjects;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public abstract sealed class ApplicationResponse<T> permits ApplicationSuccess, ApplicationError {
@@ -30,5 +31,28 @@ public abstract sealed class ApplicationResponse<T> permits ApplicationSuccess, 
 
     public boolean isSuccess() {
         return result != null;
+    }
+
+    public HttpStatus getHttpStatusFromCode() {
+        if (code.equals(EOperationCode.DOMAIN_ERROR) || code.equals(EOperationCode.INTERNAL_ERROR)) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        if (code.equals(EOperationCode.USER_INPUT_ERROR)) {
+            return HttpStatus.BAD_REQUEST;
+        }
+
+        if (code.equals(EOperationCode.DEFAULT_SUCCESS))
+            return HttpStatus.OK;
+
+        if (code.equals(EOperationCode.CREATED)) {
+            return HttpStatus.CREATED;
+        }
+
+        if (code.equals(EOperationCode.UPDATED)) {
+            return HttpStatus.OK;
+        }
+
+        return HttpStatus.OK;
     }
 }
